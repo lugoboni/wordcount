4b7435443a7dc729bab18626e4904c47182177f9
lugoboni <gustavo.lugoboni@gmail.com>
2019-07-11 14:43:52 -0300

    url created

--- word_count/word_count/core/urls.py
+++ word_count/word_count/core/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 
-from word_count.core.views import (
-)
+from word_count.core.views import (WordCountFormView)
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path("~word-count/", view=WordCountFormView, name="word-count"),
 ]
