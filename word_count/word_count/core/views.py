1bab3abe0c9989113c0b9118b17191343cc2712a
lugoboni <gustavo.lugoboni@gmail.com>
2019-07-11 14:42:30 -0300

    view created

---
+++ word_count/word_count/core/views.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.views.generic import View
+from django.views.generic.edit import FormView
+
+from ..forms import (WordCountForm,)
+from ..models import (WordCount,)
+
+class WordCountFormView(FormView):
+
+    template_name = 'word_count_form.html'
+    form_class = WordCountForm
+    initial = {}
+    http_method_names = ['get', 'post', 'head', 'options', 'trace']
+
+    def get(self, request, service, code):
+        form = self.form_class()
+
+        self.context = {'form': form}
+        return render(request, self.template_name, self.context)
+
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+                messages.success(self.request, "MLPAv4 saved")
+                return redirect(self.request.META.get('PATH_INFO'))
+
+        except ValidationError as e:
+            messages.error(self.request, str(e))
+            return redirect(self.request.META.get('PATH_INFO'))
